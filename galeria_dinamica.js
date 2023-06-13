6404b27a5a112a49963a38b7977b1c35147ed0d3
scobo04 <scobo15049@alumnes.iesmanacor.cat>
2023-06-14 01:29:21 +0200

    Actualizo la práctica

---
+++ galeria_dinamica.js
@@ -0,0 +1,12 @@
+var textos=[
+    "Mis gafas en el escritorio",
+    "Camino a la perdición",
+    "Paseando en bicicleta",
+    "Vias de el tren"
+]
+
+
+function cargarfoto(img, txt){
+    document.getElementById("galeria").src="https://picsum.photos/id/"+img+"/400/400";
+    document.getElementById("mitexto").innerHTML=textos[txt];
+}
